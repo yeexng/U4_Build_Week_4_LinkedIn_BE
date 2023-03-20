4cd20ed6bac2957eaa0642f29df9d46ef45b0158
Batı Gökovalı <batigokovali@icloud.com>
2023-03-20 16:45:46 +0300

    update

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,6 +5,7 @@ import q2m from "query-to-mongo"
 import { v2 as cloudinary } from "cloudinary"
 import { CloudinaryStorage } from "multer-storage-cloudinary"
 import { pipeline } from "stream";
+import multer from "multer"
 
 const postsRouter = express.Router()
 
@@ -12,16 +13,16 @@ const cloudinaryUploader = multer({
     storage: new CloudinaryStorage({
         cloudinary,
         params: {
-            folder: "LinkedIn-DB/posts"
+            folder: "U4-W4-BW4/posts"
         },
     }),
-}).single("poster")
+}).single("post")
 
 //POST a post
 postsRouter.post("/", async (req, res, next) => {
     try {
         const newPost = new PostsModel(req.body)
-        const { _id } = await newProduct.save()
+        const { _id } = await newPost.save()
         res.status(201).send({ _id })
     } catch (error) {
         next(error)
@@ -82,6 +83,25 @@ postsRouter.delete("/:postID", async (req, res, next) => {
         const deletedPost = await PostsModel.findByIdAndDelete(req.params.postID)
         if (deletedPost) {
             res.status(204).send()
+        } else {
+            next(createHttpError(404, `Post with ID ${req.params.postID} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+//POST an image to a post
+postsRouter.post("/:postID/image", cloudinaryUploader, async (req, res, next) => {
+    try {
+        const post = await PostsModel.findById(req.params.postID)
+        if (post) {
+            post.image = req.file.path;
+            await post.save()
+            res.status(201).send({
+                success: "true",
+                message: `Image has been added to post with id ${req.params.postID}!`
+            })
         } else {
             next(createHttpError(404, `Post with ID ${req.params.postID} not found!`))
         }
